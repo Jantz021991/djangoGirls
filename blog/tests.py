5803f7ecc5b5962df7b5c657964ec6cd22377851
Jantz021991 <djantz@unomaha.edu>
2018-03-30 22:31:45 -0500

    Updating Watson Api with Django girls blog

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-# Create your tests here.
+class Blog_ATS(unittest.TestCase):
+
+   def setUp(self):
+       self.driver = webdriver.Chrome()
+
+   def test_blog(self):
+       user = "ID"
+       pwd = "PW"
+       driver = self.driver
+       driver.maximize_window()
+       driver.get("http://127.0.0.1:8000/admin")
+       elem = driver.find_element_by_id("id_username")
+       elem.send_keys(user)
+       elem = driver.find_element_by_id("id_password")
+       elem.send_keys(pwd)
+       elem.send_keys(Keys.RETURN)
+       driver.get("http://127.0.0.1:8000")
+       assert "Logged In"
+       time.sleep(5)
+
+   def tearDown(self):
+       self.driver.close()
+
+if __name__ == "__main__":
+   unittest.main()
--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,52 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+import json
+from watson_developer_cloud import ToneAnalyzerV3
+from watson_developer_cloud import LanguageTranslatorV2 as LanguageTranslator
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    tone_analyzer = ToneAnalyzerV3(
+        username='02b20e7e-5854-404e-8f77-11990c5e5759',
+        password='MnocvW2pDKv1',
+        version='2016-05-19')
+
+    language_translator = LanguageTranslator(
+        username='95c4eb96-7340-4c84-b421-ca6733aed7a3',
+        password='XdvhvNWVuSZX')
+
+
+    # print(json.dumps(translation, indent=2, ensure_ascii=False))
+
+    for post in posts:
+        posting = post.text
+        toneObj= json.dumps(tone_analyzer.tone(tone_input=posting,
+                                   content_type="text/plain"), indent=2)
+        post.toneObj2 = json.loads(toneObj)
+        print(post.toneObj2)
+        post.angerScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][0]['score']
+        post.disgustScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][1]['score']
+        post.fearScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][2]['score']
+        post.joyScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][3]['score']
+        post.sadScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][4]['score']
+
+        translation = language_translator.translate(
+            text=post.text,
+            source='en',
+            target='es')
+        obj= json.dumps(translation, indent=2, ensure_ascii=False)
+        post.language_translation_object = json.loads(obj)
+        print(json.dumps(translation, indent=2, ensure_ascii=False))
+        post.translations = post.language_translation_object['translations']
+        post.trns_key = post.translations[0]
+        post.translation_spanish = post.trns_key['translation']
+        post.word_count = post.language_translation_object['word_count']
+        post.character_count = post.language_translation_object['character_count']
+
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
